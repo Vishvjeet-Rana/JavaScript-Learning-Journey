8cbb4d06e19f1903f206f7162a362f7d6be01316
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-18 15:02:10 +0530

    unary operators start

---
+++ part1/operators.js
@@ -0,0 +1,11 @@
+// Arithmetic operators
+let a = 10;
+let b = 78;
+console.log(`addition is = ${a + b}`);
+console.log(`subtraction is = ${b - a}`);
+console.log(`multiplication is = ${a * b}`);
+console.log(`division is = ${b / a}`);
+console.log(`remainder is = ${b % a}`);
+console.log(`exponention or power is = ${b ** 10}`);
+
+// Unary operators (++ , --)
