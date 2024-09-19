3b3a85ed15f8c022bf5e9291ad08161a9e104c30
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 11:38:28 +0530

    unary operators done

--- part1/operators.js
+++ part1/operators.js
@@ -9,3 +9,10 @@ console.log(`remainder is = ${b % a}`);
 console.log(`exponention or power is = ${b ** 10}`);
 
 // Unary operators (++ , --)
+let age = 35;
+// In pre-incerement and pre-decrement change the value then use/assign
+console.log(`Pre-increment the age ${++age}`);
+console.log(`Pre-decrement the age ${--age}`);
+// In post-incerement and post-decrement use/assign the value then change
+console.log(`Post-increment the age ${age++}`);
+console.log(`Post-decrement the age ${age--}`);
