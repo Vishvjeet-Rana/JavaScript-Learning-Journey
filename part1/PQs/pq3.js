32adb600881866e26d34afbb4077f7fde1737c58
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 12:50:17 +0530

    DONE

---
+++ part1/PQs/pq3.js
@@ -0,0 +1,22 @@
+// Q.1 declare name as string and print its length
+let fullName = "Vishvjeet Rana";
+console.log(fullName.length);
+
+// Q.2 declare string and print its first character
+let greet = "hello there";
+console.log(greet[0]);
+
+// Q.3 declare string and print its last character
+let profession = "developer";
+console.log(profession[profession.length - 1]);
+
+// Q.4 find ouput of following code
+let learningSrc = "apnacollege";
+console.log(learningSrc + 123); // apnacollege123
+console.log(learningSrc - 123); // NaN
+
+// Q.5
+let emp = "";
+let space = " ";
+console.log(emp.length); // 0
+console.log(space.length); // 1
