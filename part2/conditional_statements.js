9f8d3819c6a7ab37104fc2dc6b6704686a169745
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 16:16:24 +0530

    practice ques 1 done

---
+++ part2/conditional_statements.js
@@ -0,0 +1,16 @@
+// 'if' statement
+// example 1
+let name = "hello";
+console.log("before the if statement");
+if (name) {
+  console.log(`name is ${name}`);
+}
+console.log("after the if statement");
+
+//example 2
+let age = prompt("enter you age");
+if (age >= 18) {
+  console.log("you can drive");
+} else {
+  console.log("you can't drive");
+}
---
+++ part2/pq1.js
@@ -0,0 +1,8 @@
+let signal = prompt("what is traffic light color ?");
+if (signal == "red") {
+  console.log("stop");
+} else if (signal == "yellow") {
+  console.log("slow down");
+} else {
+  console.log("you can go");
+}
