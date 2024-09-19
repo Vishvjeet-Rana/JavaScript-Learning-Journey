9fca1964f70a8127bc9106509da7221b683e9540
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 17:45:26 +0530

    practice ques 4 done

---
+++ part2/pq4.js
@@ -0,0 +1,29 @@
+let day = prompt("enter the number of day");
+
+switch (day) {
+  case "1":
+    alert("Its Monday babes!!");
+    break;
+  case "2":
+    alert("Its Tuesday babes!!");
+    break;
+  case "3":
+    alert("Its Wednesday babes!!");
+    break;
+  case "4":
+    alert("Its Thursday babes!!");
+    break;
+  case "5":
+    alert("Its Friday babes!!");
+    break;
+  case "6":
+    alert("Its Saturday babes!!");
+    break;
+  case "7":
+    alert("Its Sunday babes!!");
+    break;
+
+  default:
+    alert("number is out of range!!");
+    break;
+}
