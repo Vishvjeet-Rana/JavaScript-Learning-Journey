17fc78f985c51a628c55f0bb5ca4fdd14788a95d
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 12:35:27 +0530

    string data type done

---
+++ part1/string_datatype.js
@@ -0,0 +1,15 @@
+// String indices
+let name = "Peter Parker";
+console.log(name[2]);
+
+// Concatenation of strings
+let firstName = "Vishvjeet";
+let lastName = "Rana";
+fullName = firstName + lastName;
+
+//extra information
+console.log(firstName + " " + lastName); // + defines the concatenation
+// OR
+console.log(`full name is = ${firstName} ${lastName}`); // no need of giving the extra space separately
+
+console.log(fullName.length);
