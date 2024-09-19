0811740a82bca619cd83ce97a8ae97e1e0b62d95
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 21:13:14 +0530

    lowerCase & upperCase done

---
+++ part3/String_Methods/upper_lower.js
@@ -0,0 +1,12 @@
+// Upper case
+
+let course = "information technology";
+let course_upperCase = course.toUpperCase();
+console.log(course_upperCase);
+
+// Lower case
+let language = "JavaScript AND JAVA";
+let language_lowerCase = language.toLowerCase();
+console.log(language_lowerCase);
+
+// both methods returns a new string
