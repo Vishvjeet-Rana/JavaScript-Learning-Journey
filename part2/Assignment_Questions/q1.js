ccbd803edf85d6b138ef45acfd1243fa0bb97ebc
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 17:53:48 +0530

    q1 done

---
+++ part2/Assignment_Questions/q1.js
@@ -0,0 +1,9 @@
+// # Create a number variable num with some value.
+//Now, print "good" if the number is divisible by 10 and print "bad" if it is not.
+
+let num = prompt("enter the number:");
+if (num % 10 === 0) {
+  console.log("good number");
+} else {
+  console.log("bad number");
+}
