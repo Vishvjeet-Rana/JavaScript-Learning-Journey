31d4fac4f94c894d3c5057eaa0d8f4361b194c31
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 16:35:20 +0530

    nested if statement done

--- part2/conditional_statements.js
+++ part2/conditional_statements.js
@@ -9,9 +9,23 @@ console.log("after the if statement");
 
 // 'else' statement
 //example 2
-let age = prompt("enter you age");
-if (age >= 18) {
-  console.log("you can drive");
+// let age = prompt("enter you age");
+// if (age >= 18) {
+//   console.log("you can drive");
+// } else {
+//   console.log("you can't drive");
+// }
+
+//nested if-else statements
+let marks = 890;
+
+if (marks >= 33) {
+  console.log("Passed");
+  if (marks >= 80 && marks <= 90) {
+    console.log("merit");
+  } else if (marks >= 90) {
+    console.log("high merit");
+  }
 } else {
-  console.log("you can't drive");
+  console.log("better luck next time!!");
 }
