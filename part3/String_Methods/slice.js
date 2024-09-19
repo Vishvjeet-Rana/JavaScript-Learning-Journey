b46e933ca79f39880b7d8867cdda95cdc7b36c76
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 21:56:44 +0530

    done

---
+++ part3/String_Methods/slice.js
@@ -0,0 +1,11 @@
+let youtube = "apnacollege";
+let edited = youtube.slice(4, 8);
+console.log(edited);
+
+// if we pass -ve index then it will be calculate as string.length -  the index we had passed
+// for instance
+let youtube2 = "Hitesh Chaudhary";
+console.log(youtube2.length);
+
+let modified = youtube2.slice(-3);
+console.log(modified);
