5fceb5642004475eb3efa3b0186a68a06aab2399
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 16:20:23 +0530

    use === instead ==

--- part2/pq1.js
+++ part2/pq1.js
@@ -1,7 +1,7 @@
 let signal = prompt("what is traffic light color ?");
-if (signal == "red") {
+if (signal === "red") {
   console.log("stop");
-} else if (signal == "yellow") {
+} else if (signal === "yellow") {
   console.log("slow down");
 } else {
   console.log("you can go");
