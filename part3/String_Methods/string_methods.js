c9ad631ad7f30b7e862cf6c6de8ccb60d88360ba
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-19 21:08:44 +0530

    trim method done

--- part3/String_Methods/string_methods.js
+++ part3/String_Methods/string_methods.js
@@ -1,1 +1,11 @@
 // Trim method
+let str = "         Vishvjeet Rana ";
+let newStr = str.trim();
+// but original string str remains the same  "         Vishvjeet Rana "
+
+// trim method returns a new string. Does'nt changes the original string. Strings are immutable.
+
+// for instance in passwords we dont accept the space so we use trim method there.
+let password = prompt("enter your password");
+let edited_password = password.trim();
+console.log(`edited password = ${edited_password}`);
