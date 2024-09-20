b2631370d9c70e8354f71661918f591f76e3c4d6
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-20 11:27:04 +0530

    replace repeat method done

---
+++ part3/String_Methods/replace_repeat.js
@@ -0,0 +1,16 @@
+// Replace method
+
+let string = "Janta High School";
+let new_string = string.replace("Janta", "Maharana Pratap");
+console.log(new_string);
+
+// only first occurence changes - for instance see below
+let str = "Hello Hello Mr. John Doe";
+let newStr = str.replace("Hello", "Hii");
+console.log(newStr); //Hii Hello Mr. John Doe
+
+// Repeat method
+
+let vege = "Tomato";
+let newVege = vege.repeat(2);
+console.log(newVege);
