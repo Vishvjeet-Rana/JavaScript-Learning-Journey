c0df79d6324e4dea0e430a0a6a940299a765b01a
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-20 14:33:13 +0530

    nested array done

---
+++ part3/Array_DS/nested_arrays.js
@@ -0,0 +1,16 @@
+let arr = [
+  [1, 2],
+  [67, 23],
+  [56, 90],
+  [14, 73],
+];
+console.log(arr);
+console.log(arr[0]); //[1,2]
+console.log(arr[1]); //[67,23]
+console.log(arr[2]); //[56,90]
+console.log(arr[3]); //[14,73]
+
+console.log(arr[0][1]); //2
+console.log(arr[1][0]); //67
+console.log(arr[2][0]); //56
+console.log(arr[3][1]); //73
