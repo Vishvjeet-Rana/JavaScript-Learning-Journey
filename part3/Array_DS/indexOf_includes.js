92ef12800b3817103f6e9742391734c083a1c408
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-20 12:29:09 +0530

    indexOf includes methods done

---
+++ part3/Array_DS/indexOf_includes.js
@@ -0,0 +1,10 @@
+let colors = ["red", "green", "blue", "violet", "pink"];
+
+// indexOf gives index of ele if present otherwise gives -1
+console.log(colors.indexOf("blue")); //2
+console.log(colors.indexOf("pink")); //4
+console.log(colors.indexOf("yellow")); // -1
+
+// includes method checks if the entered ele presents in array or not
+console.log(colors.includes("yellow")); //false
+console.log(colors.includes("violet")); // true
