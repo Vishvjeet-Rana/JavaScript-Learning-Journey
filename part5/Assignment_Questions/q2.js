386f2a89e3ee3bd57636a45298321fdd6b5486eb
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-23 17:07:07 +0530

    q2 done

---
+++ part5/Assignment_Questions/q2.js
@@ -0,0 +1,12 @@
+// Create an object representing a car that stores the following properties for the car:
+// name, model, color.
+// Print the car’s name.
+
+let car = {
+  name: "Rolls Royce",
+  model: "Ghost Tail",
+  color: "Purple",
+};
+
+console.log(car.name);
+console.log(car["name"]);
