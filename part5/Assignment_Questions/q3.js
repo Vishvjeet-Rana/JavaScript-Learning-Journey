f4ff9645ef488c67ba93d347dd10f3c2f2784f2f
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-23 17:10:05 +0530

    Part 5 DONE

---
+++ part5/Assignment_Questions/q3.js
@@ -0,0 +1,17 @@
+// Create an object Person with their name, age, and city.
+// Edit their city's original value to change it to "New York".
+// Add a new property country and set it to "United States".
+
+let Person = {
+  name: "John Doe",
+  age: 768957389,
+  city: "California",
+};
+
+console.log(Person);
+
+Person.city = "New York";
+console.log(Person);
+
+Person.country = "United States";
+console.log(Person);
