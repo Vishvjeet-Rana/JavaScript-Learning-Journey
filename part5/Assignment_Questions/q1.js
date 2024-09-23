2f84ed7100a887b3f41f74ebc5df16494c837ee7
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-23 17:04:40 +0530

    q1 done

---
+++ part5/Assignment_Questions/q1.js
@@ -0,0 +1,6 @@
+// Create a program that generates a random number representing a dice roll.
+// The numbers should be between 1 and 6.
+
+let dice = Math.floor(Math.random() * 6) + 1;
+
+console.log(dice);
