8a20ba98e11a1de9e749dcb2f9bb1b292b7badaa
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-23 16:29:56 +0530

    math objects and methods done

---
+++ part5/math_object.js
@@ -0,0 +1,20 @@
+// Math.abs(x): Returns the absolute value of x, removing any negative sign.
+console.log(Math.abs(-5.4));
+console.log(Math.abs(-7.8));
+
+// Math.floor(x): Rounds x down to the nearest integer.
+console.log(Math.floor(5.9999));
+console.log(Math.floor(6.000001));
+console.log(Math.floor(-8.23456));
+
+// Math.ceil(x): Rounds x up to the nearest integer.
+console.log(Math.ceil(-4.999));
+console.log(Math.ceil(6.32));
+
+// Math.random(): Generates a pseudo-random floating-point number between 0 (inclusive) and 1 (exclusive).
+console.log(Math.random() * 10 + 1); // will generate till 10
+console.log(Math.random()); // 0 to 1 (exclusive)
+
+// Math.pow(base, exponent): Raises base to the power of exponent and returns the result.
+console.log(Math.pow(2, 6));
+console.log(Math.pow(67, 43));
