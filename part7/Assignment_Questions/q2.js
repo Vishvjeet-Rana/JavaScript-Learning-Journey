33fd5db9cff09b89eff6a489ea15d7412695e882
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-25 13:41:18 +0530

    check number even or not

---
+++ part7/Assignment_Questions/q2.js
@@ -0,0 +1,8 @@
+// Write an arrow function named isEven() that takes a single number as argument and returns if it is even or not
+
+const isEven = (n) => {
+  return n % 2 == 0;
+};
+
+console.log(isEven(10));
+console.log(isEven(63));
