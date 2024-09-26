f05d790967908fd30ba467dd7f963ed4f410ea59
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-26 14:25:50 +0530

    q3 done

---
+++ part8/Assignment_Questions/q3.js
@@ -0,0 +1,7 @@
+// Create a new array whose elements are in uppercase of words present in the original array
+
+let strArr = ["my", "country", "is", "best"];
+
+const newStrArr = strArr.map((string) => string.toUpperCase());
+
+console.log(newStrArr);
