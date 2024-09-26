8d9e009accd8271083df631798620767e40c02c6
Vishvjeet-Rana <aryanrana62366@gmail.com>
2024-09-26 14:23:13 +0530

    q2 done

---
+++ part8/Assignment_Questions/q2.js
@@ -0,0 +1,7 @@
+// Create a new array using the map function whose each element is equal to the original element plus 5
+
+let arr = [50, 60, 70, 85, 100];
+
+const newArr = arr.map((el) => el + 5);
+
+console.log(newArr);
